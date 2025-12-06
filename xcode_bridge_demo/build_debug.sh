8d8fe7332616d8655fcbbeff8332bf3f918ecd7c
Keith Hetrick <keithjhetrick@gmail.com>
2025-12-06 14:22:04 -0600

    Remove JUCE/Xcode demos

--- xcode_bridge_demo/build_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-set -euo pipefail
-SCRIPT_DIR="$(cd -- "$(dirname "$0")" && pwd)"
-cmake -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -G Xcode
-cmake --build "$SCRIPT_DIR/build" --config Debug
